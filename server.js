f03a1fd0904e5fb4745b956543c2c8786bc1280c
vineet1603 <offi_avenger@outlook.com>
2026-05-21 19:38:49 +0530

    Fixed PORT variable for Render deployment

--- server.js
+++ server.js
@@ -158,5 +158,5 @@ app.get('/api/seed', async (req, res) => {
     } catch (error) { res.status(500).send("❌ Error"); }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
